0744bd006ed23f6cdc3db8ab96136e5c05d09477
Sheetal <gajare.sheetal@gmail.com>
2026-03-20 13:25:00 +1100

    Added new project with intial set-up

---
+++ Src/LongestSubsequence/LongestSubsequence.ConsoleApp/Program.cs
@@ -0,0 +1,5 @@
+﻿using LongestSubsequence.ConsoleApp.Utilities;
+
+Console.Write("Enter integers separated by spaces: ");
+var input = Console.ReadLine();
+Console.WriteLine($"Longest increasing subsequence: {input}");
---
+++ Src/LongestSubsequence/LongestSubsequence.ConsoleApp/Utilities/LongestSubsequenceFinder.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LongestSubsequence.ConsoleApp.Utilities
+{
+    public static class LongestSubsequenceFinder
+    {
+        public static string FindLongestSubsequence(string input)
+        {
+         throw new NotImplementedException();
+        }
+    }
+}
