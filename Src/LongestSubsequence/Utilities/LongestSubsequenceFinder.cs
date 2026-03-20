cbb03152a9ae7fa1c839f528e77382dfcbffbba9
Sheetal <gajare.sheetal@gmail.com>
2026-03-20 14:46:39 +1100

    added finding start index for longest sequence and length using 2 pointer approach

--- Src/LongestSubsequence/Utilities/LongestSubsequenceFinder.cs
+++ Src/LongestSubsequence/Utilities/LongestSubsequenceFinder.cs
@@ -1,4 +1,6 @@
-﻿namespace Utilities
+﻿using System.Diagnostics;
+
+namespace Utilities
 {
     public class LongestSubsequenceFinder
     {
@@ -9,8 +11,21 @@
                 throw new ArgumentNullException("Input cannot be empty or whitespace.");
             }
             var numbers = ConvertToIntArray(input);
-            string result = string.Concat(numbers.Select(x => x.ToString() + " ")).TrimEnd();
-            return result;
+            if (numbers.Length == 0)
+            {
+                throw new ArgumentException("Input must contain at least one integer.", nameof(input));
+            }
+            if (numbers.Length == 1)
+            {
+                return numbers[0].ToString();
+            }
+
+            var (start, length) = GetLongestRunIndex(numbers);
+
+            //Console.WriteLine($"Longest run starts at index {start} with length {length}.");
+            //return string.Concat(numbers.Select(x => x.ToString() + " ")).TrimEnd();
+
+            return FormatOutput(numbers, start, length); ;
         }
 
 
@@ -27,6 +42,55 @@
             }
             return result;
 
+        }
+
+        internal static string FormatOutput(int[] nums, int start, int length)
+        {
+            var parts = new string[length];
+
+            for (var i = 0; i < length; i++)
+            {
+                parts[i] = nums[start + i].ToString();
+            }
+
+            return string.Join(" ", parts);
+        }
+
+        //using the two pointer technique to find the longest run of increasing integers in the array
+        internal static (int Start, int Length) GetLongestRunIndex(int[] nums)
+        {
+            var startPointer = 0;
+            var endPointer = 1;
+            var currentStart = 0;
+            var currentLength = 1;
+
+            for (var i = 1; i < nums.Length; i++)
+            {
+                if (nums[i] > nums[i - 1])
+                {
+                    currentLength++;
+                }
+                else
+                {
+                    if (currentLength > endPointer)
+                    {
+                        endPointer = currentLength;
+                        startPointer = currentStart;
+                    }
+
+                    currentStart = i;
+                    currentLength = 1;
+                }
+            }
+
+            // Check the final run
+            if (currentLength > endPointer)
+            {
+                endPointer = currentLength;
+                startPointer = currentStart;
+            }
+
+            return (startPointer, endPointer);
         }
     }
 }
