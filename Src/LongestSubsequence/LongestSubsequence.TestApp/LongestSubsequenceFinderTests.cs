555d7ce04ca798c0114d09fefe57a256ee201082
Sheetal <gajare.sheetal@gmail.com>
2026-03-20 15:16:08 +1100

    added unit test on test case 1, 2  and 3

--- Src/LongestSubsequence/LongestSubsequence.TestApp/LongestSubsequenceFinderTests.cs
+++ Src/LongestSubsequence/LongestSubsequence.TestApp/LongestSubsequenceFinderTests.cs
@@ -3,20 +3,36 @@ namespace LongestSubsequence.TestApp
 {
     public class LongestSubsequenceFinderTests
     {
-        private static string LoadTestData()
+        private static string LoadTestDataFile(string filename)
         {
-            return "5 2 3 4 5 8 4 7 6";
+            var path = Path.GetFullPath(Path.Combine("..", "..", "..", "TestFiles", filename));
+            return File.ReadAllText(path).Trim();
         }
 
         [Fact]
-        public void TestCase01_SimpleSequenceOfInteger()
+        public void TestCase01()
         {
             var input = "6 1 5 9 2";
             var expected = "1 5 9";
             var result = LongestSubsequenceFinder.FindLongestSubsequence(input);
             Assert.Equal(expected, result);
         }
-
+        [Fact]
+        public void TestCase02()
+        {
+            var input = LoadTestDataFile("TestCase2_Input.txt");
+            var expected = LoadTestDataFile("TestCase2_Output.txt");
+            var result = LongestSubsequenceFinder.FindLongestSubsequence(input);
+            Assert.Equal(expected, result);
+        }
+        [Fact]
+        public void TestCase03()
+        {
+            var input = LoadTestDataFile("TestCase3_Input.txt");
+            var expected = LoadTestDataFile("TestCase3_Output.txt");
+            var result = LongestSubsequenceFinder.FindLongestSubsequence(input);
+            Assert.Equal(expected, result);
+        }
 
     }
 }
